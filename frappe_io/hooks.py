4fdf8f18ab9c3a42a0e2ecf5a248080c9b17c87e
Khaled Ahmed <k.ahmed@inet.works>
2019-05-18 01:58:06 +0000

    init

---
+++ frappe_io/hooks.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "frappe_io"
+app_title = "Frappe Framework"
+app_publisher = "Frappe"
+app_description = "frappe.io"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "info@frappe.io"
+app_version = "0.0.1"
+
+# not required for in-app help
+# required_apps = ["frappe_theme"]
+
+fixtures = ["Contact Us Settings", "Web Form"]
+
+website_context = {
+	"repo": "frappe/frappe_io",
+	"brand_html": "<img class='mr-2 d-inline-block align-top' src='/assets/frappe_theme/img/frappe-logo.png' width='30px' height='30px' />Frappe",
+	"top_bar_items": [
+		{"label": "Products", "right": 1, "child_items": [
+			{"label": "Frappe Framework", "url":"/frappe"},
+			{"label": "ERPNext", "url":"/erpnext"},
+			{"label": "Bench", "url":"/bench"},
+			{"label": "Gantt", "url":"/gantt"},
+			{"label": "Charts", "url":"/charts"},
+			{"label": "DataTable", "url":"/datatable"},
+		]},
+		{"label": "About", "right": 1, "child_items": [
+			{"label": "Team", "url":"/about"},
+			{"label": "Jobs", "url":"/jobs"},
+			{"label": "Inspiration", "url":"/inspiration"},
+			{"label": "Story", "url":"/story"},
+			{"label": "Contact", "url":"/about#contact"},
+		]},
+		{"label": "Blog", "url":"/blog", "right": 1},
+	],
+	"favicon": "/assets/frappe_theme/img/frappe-logo.png",
+	"hide_login": 1
+}
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/frappe_io/css/frappe_io.css"
+# app_include_js = "/assets/frappe_io/js/frappe_io.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/frappe_io/css/frappe_io.css"
+web_include_js = "/assets/frappe_io/js/docs.js"
+look_for_sidebar_json = True
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "frappe_io.install.before_install"
+# after_install = "frappe_io.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "frappe_io.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"frappe_io.tasks.all"
+# 	],
+# 	"daily": [
+# 		"frappe_io.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"frappe_io.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"frappe_io.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"frappe_io.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "frappe_io.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "frappe_io.event.get_events"
+# }
---
+++ frappe_io/www/about.py
@@ -0,0 +1,153 @@
+
+def get_context(context):
+	context.team = [
+		{
+			'name': 'Rushabh Mehta',
+			'role': 'Founder and CEO',
+			'twitter': 'rushabh_mehta',
+			'linkedin': 'https://in.linkedin.com/in/rushabhmehta1',
+			'blogger': 'rushabh'
+		},
+		{
+			'name': 'Nabin Hait',
+			'role': 'Chief Operations Officer',
+			'twitter': 'nabinhait',
+			'blogger': 'nabin'
+		},
+		{
+			'name': 'Umair Sayed',
+			'role': 'Chief Customer Officer',
+			'twitter': 'umairsTWT',
+			'blogger': 'umair'
+		},
+		{
+			'name': 'Prakash Hodage',
+			'role': 'Admin + Accounts',
+			'blogger': 'prakash',
+			'linkedin': 'https://in.linkedin.com/in/prakash-hodage-6702b231'
+		},
+		{
+			'name': 'Saurabh Palande',
+			'role': 'Developer',
+			'twitter': 'saurabh6790',
+			'blogger': 'saurabh'
+		},
+		{
+			'name': 'Rohit Waghchaure',
+			'role': 'Developer',
+			'twitter': 'rohitw1991',
+			'blogger': 'rohit'
+		},
+		{
+			'name': 'Faris Ansari',
+			'role': 'Developer',
+			'twitter': 'netchampfaris',
+			'blogger': 'faris'
+		},
+		{
+			'name': 'Prateeksha Singh',
+			'role': 'Developer',
+			'blogger': 'prateeksha',
+			'twitter': 'pratu16x7'
+		},
+		{
+			'name': 'Shreya Shah',
+			'role': 'Developer',
+			'twitter': 'shreyashah115'
+		},
+		{
+			'name': 'Zarrar Shaikh',
+			'role': 'Developer',
+			'medium': 'zlash65',
+			'twitter': 'zlash65',
+			'github': 'zlash65'
+		},
+		{
+			'name': 'Ameya Shenoy',
+			'role': 'Developer',
+			'medium': 'codingCoffee',
+			'twitter': 'coding_caffeine'
+		},
+		{
+			'name': 'Kenneth Sequeira',
+			'role': 'Marketing Engineer',
+			'twitter': 'batkenobi',
+		},
+		{
+			'name': 'Suraj Shetty',
+			'role': 'Developer',
+			'medium': 'surajshetty3416',
+			'twitter': 'surajshetty3416',
+			'github': 'surajshetty3416'
+		},
+		{
+			'name': 'Rushikesh Parekh',
+			'role': 'Business Analyst',
+			'twitter': 'rushikeshparekh',
+		},
+		{
+			'name': 'Basawaraj',
+			'role': 'Head of Enterprise sales',
+			'medium': 'basawarajsavalagi',
+		},
+		{
+			'name': 'Aditya Hase',
+			'role': 'Developer',
+			'github': 'adityahase',
+			'linkedin': 'https://in.linkedin.com/in/adityahase',
+		},
+		{
+			'name': 'Mitali Deshpande',
+			'role': 'Business Analyst',
+			'linkedin': 'https://in.linkedin.com/in/mitali-deshpande-571a8416'
+		},
+		{
+			'name': 'Salman Mulani',
+			'role': 'Business Analyst',
+			'linkedin': 'https://in.linkedin.com/in/salman-mulani-7a9bb659'
+		},
+		{
+			'name': 'Chinmay Pai',
+			'role': 'Developer',
+			'github': 'thunderbottom'
+		},
+		{
+			'name': 'Jai Chavan',
+			'role': 'Business Analyst',
+			'linkedin': 'https://in.linkedin.com/in/jai-chavan-a894a3149'
+		},
+		{
+			'name': 'Mangesh Khairnar',
+			'role': 'Developer',
+			'github': 'mangesh-khairnar'
+		},
+		{
+			'name': 'Sahil Khan',
+			'role': 'Developer',
+			'github': 'sahil28297'
+		},
+		{
+			'name': 'Deepesh Garg',
+			'role': 'Developer',
+			'twitter': 'DeepeshGarg16',
+		},
+		{
+			'name': 'Ketan Desai',
+			'role': 'VP - Channel and Alliance',
+			'linkedin': 'https://in.linkedin.com/in/ketandesai9'
+		},
+		{
+			'name': 'Shivam Mishra',
+			'role': 'Developer and Advocate',
+			'twitter': 'scmmishra'
+		},
+		{
+			'name': 'Anurag Mishra',
+			'role': 'Developer',
+		},
+		{
+			'name': 'Himanshu Warekar',
+			'role': 'Developer',
+			'github': 'himanshu-96'
+		}
+	]
